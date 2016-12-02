be0d9bbfab826168acc082e68088d34812c52812
Drew Holt <drew@liatrio.com>
2016-12-01 21:53:51 -0700

    LTA-42 pin tomcat

--- Berksfile
+++ Berksfile
@@ -2,6 +2,4 @@ source 'https://supermarket.chef.io'
 
 metadata
 
-group :testing do
-  cookbook 'minitest-handler', '~> 1.5.0'
-end
+cookbook 'tomcat'
--- metadata.rb
+++ metadata.rb
@@ -7,4 +7,4 @@ long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.1'
 
 depends 'java' # '~> 1.39.0'
-depends 'tomcat' # '~> 2.0.2'
+depends 'tomcat' '~> 2.0.2'

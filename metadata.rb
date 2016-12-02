67c8d8d46e84f094b1506758002dfd14bcfefbdf
Drew Holt <drew@liatrio.com>
2016-12-01 21:32:02 -0700

    LTA-42 increment cookbook version manually

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'drew@liatrio.com'
 license          'All rights reserved'
 description      'Wrapper cookbook that Installs/Configures archiva-liatrio'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
+version          '0.1.1'
 
 depends 'java' # '~> 1.39.0'
 depends 'tomcat' # '~> 2.0.2'

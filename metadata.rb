2430ccb8ec3892ea2a9d63b68904dcf787771f79
Drew Holt <drew@liatrio.com>
2016-12-01 22:01:31 -0700

    LTA-42 pin java

--- metadata.rb
+++ metadata.rb
@@ -6,5 +6,5 @@ description      'Wrapper cookbook that Installs/Configures archiva-liatrio'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.1'
 
-depends 'java' # '~> 1.39.0'
+depends 'java', '~> 1.39.0'
 depends 'tomcat' '~> 2.0.2'
